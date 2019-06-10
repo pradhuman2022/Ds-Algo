81b82a4d752c6d7dd762ddaca6b8afbc4c118c8a
pradhuman2022 <42096528+pradhuman2022@users.noreply.github.com>
2019-06-10 11:21:21 +0530

    Create HasAPath.java
    Given an undirected graph G(V, E) and two vertices v1 and v2(as integers), check if there exists any path between them or not. Print true or false.
    V is the number of vertices present in graph G and vertices are numbered from 0 to V-1.
    E is the number of edges present in graph G.
    Input Format :
    Line 1: Two Integers V and E (separated by space)
    Next E lines : Two integers a and b, denoting that there exists an edge between vertex a and vertex b (separated by space)
    Line (E+2) : Two integers v1 and v2 (separated by space)
    Output Format :
    true or false
    Constraints :
    2 <= V <= 1000
    1 <= E <= 1000
    0 <= v1, v2 <= V-1
    Sample Input 1 :
    4 4
    0 1
    0 3
    1 2
    2 3
    1 3
    Sample Output 1 :
    true
    Sample Input 2 :
    6 3
    5 3
    0 1
    3 4
    0 3
    Sample Output 2 :
    false

---
+++ Graph1/HasAPath.java
@@ -0,0 +1,66 @@
+import java.util.* ;
+
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int V = sc.nextInt();
+		int E = sc.nextInt();
+        int visited[] = new int[V] ;
+        
+        Graph g = new Graph(V) ;
+        for(int i = 0 ; i < E ; i++)
+        {
+          g.addEdges(sc.nextInt(), sc.nextInt()) ;  
+        }
+        for(ArrayList a : g.adList)
+        {
+            Collections.sort(a) ;
+        }
+        System.out.println(g.bfs(sc.nextInt(), sc.nextInt(), V, visited)) ;
+		/* Write Your Code Here
+		 * Complete the Rest of the Program
+		 * You have to take input and print the output yourself
+		 */
+	}
+    static class Graph 
+    {
+        ArrayList<ArrayList<Integer>> adList ; 
+        public  Graph(int V)
+        {
+            adList = new ArrayList<ArrayList<Integer>>();
+            for(int i = 0 ; i < V ; i++)
+            {
+                adList.add(new ArrayList<Integer>()) ;
+            }
+        }
+        void addEdges(int a, int b)
+        {
+            adList.get(a).add(b) ;
+            adList.get(b).add(a) ;
+        }
+       boolean bfs(int start , int end, int V, int visited[])
+       {
+           Queue<Integer> q = new ArrayDeque<Integer>() ;
+           q.add(start) ;
+           while(!q.isEmpty())
+           {
+               
+               int current = q.poll() ;
+               if(visited[current] == 0)
+               {
+               if(current == end)
+                return true ;
+               visited[current] = 1 ;    
+               for(int x : adList.get(current))
+               {
+                  q.offer(x) ; 
+               }
+               }
+           }
+           return false ;
+       }
+    }
+   
+}
