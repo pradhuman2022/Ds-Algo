e1e92bb90633afc4343ac63b754c9d13ab6daf7f
pradhuman2022 <42096528+pradhuman2022@users.noreply.github.com>
2019-09-06 19:50:14 +0530

    Update DataStream.java

--- LeetCode/Heaps/DataStream.java
+++ LeetCode/Heaps/DataStream.java
@@ -7,7 +7,19 @@ class MedianFinder {
         bg = new PriorityQueue<Integer>((a, b) -> a - b);
         sm = new PriorityQueue<Integer>((a, b)-> b - a);
         }
-    
+        /*
+		
+so we need to maintain the order of numbers we use min heap for one half and max heap for one half , 
+	suppose the numbers are 2 4 3 5 
+	then 
+	max heap => 2,
+	min heap => 3, 4 , 5 
+	but here we need to return median as length is even so when diff of len is 2 then 
+	we can say that we need to move one element from one half to other half ! 
+ so min heap => 2, 3  
+max heap => 4, 5 
+then we will return 3 + 4 / 2 as answer ;
+*/
     public void addNum(int num) {
         
         if(bg.size() == 0 && sm.size() == 0)
