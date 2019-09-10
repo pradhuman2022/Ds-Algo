61caa9b0ddc5b2a37af44063b160c31c66696a24
pradhuman2022 <42096528+pradhuman2022@users.noreply.github.com>
2019-09-10 22:33:34 +0530

    Rearrange Letters
    https://practice.geeksforgeeks.org/problems/rearrange-characters/0

---
+++ GeeksForGeeks/Heap/REArrangeLetter.java
@@ -0,0 +1,36 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+		Scanner sc = new Scanner(System.in) ; 
+		int T = sc.nextInt() ;
+		sc.nextLine() ;
+		
+		while(T-- > 0)
+		{
+		  String s = sc.nextLine() ;    
+		  System.out.println(isPossible(s)) ;   
+		}
+	}
+	
+	static int isPossible(String s)
+	{
+	    Queue<pair> pq = new PriorityQueue<pair>((a, b)-> b.count - a.count) ;
+	    int count1[] = new int[26] ;
+	    int n = s.length() ;
+	    
+	    for(int i = 0 ; i < n; i++)
+	    {
+	        count1[s.charAt(i) - 'a']++ ;
+	        if(count1[s.charAt(i) - 'a'] > (n + 1) / 2)
+	          return  0 ;
+	    }
+	   return 1 ;     
+	}
+
+}
