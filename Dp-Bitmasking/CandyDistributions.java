ceb373948fe6d485bdc90d5fa2c5137a4c1ae003
Pradhuman Shankar <42096528+pradhuman2022@users.noreply.github.com>
2019-12-04 15:41:16 +0530

    Create CandyDistributions.java
    Kan is a trainer at  college . To reward his n students he bought a packet of N candies all with different flavours. But the problem is some students like certain flavour while some doesn't. Now kan wants to know the number of ways he can distribute these n candies to his n students such that every student gets exactly one candy he likes.

---
+++ Dp-Bitmasking/CandyDistributions.java
@@ -0,0 +1,29 @@
+
+public class solution {
+	
+	long solve(int[][] chocos,int N){
+		
+		//Write your code here.
+		
+		long dp[] = new long[1 << N] ; 
+		for(int i = 0 ; i < dp.length ; i++)
+		 dp[i] = Integer.MAX_VALUE ;
+	     return waysToDist(chocos, 0, N, dp, 0) ;
+	}
+	
+	static long waysToDist(int chocos[][], int mask, int n, long dp[], int Sn){
+	    if(mask == (1 << n) - 1)
+	      return 1 ;
+	    
+	    if(dp[mask] != Integer.MAX_VALUE)
+	      return dp[mask] ; 
+	     
+	    long totalWays = 0 ;  
+	    for(int i = 0 ; i < n ; i++){
+	        if((mask & (1 << i)) == 0 && chocos[Sn][i] == 1)
+	         totalWays += waysToDist(chocos, mask|(1 << i), n, dp, Sn + 1) ;
+	    }  
+	    return dp[mask] = totalWays ;
+	}
+	
+}
