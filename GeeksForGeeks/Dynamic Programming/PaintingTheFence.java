1098278e3678e2b4d82f435b49f7be53876977d6
Pradhuman Shankar <42096528+pradhuman2022@users.noreply.github.com>
2020-01-27 01:12:10 +0530

    Create PaintingTheFence.java
    https://practice.geeksforgeeks.org/problems/painting-the-fence/

---
+++ GeeksForGeeks/Dynamic Programming/PaintingTheFence.java
@@ -0,0 +1,35 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    static int mod = (int)Math.pow(10,9) + 7 ; 
+    
+	public static void main (String[] args) {
+		//code
+		Scanner sc = new Scanner(System.in) ; 
+		int T = sc.nextInt() ; 
+		while(T-->0){
+		  int n = sc.nextInt() ; 
+		  int k = sc.nextInt() ;
+		  System.out.println(paintingTheFence(n, k));
+		}
+	}
+	static long paintingTheFence(int n, int k){
+	    
+	    long dp[] = new long[n + 1] ; 
+	    long diff = k;
+	    dp[1] = k ; 
+	    long same = 0;
+	    for(int i = 2 ; i <= n; i++){
+	        same = diff ; 
+	        diff = (k - 1) * dp[i - 1] ; 
+	        diff %= mod ;
+	        dp[i] = same + diff ; 
+	        dp[i] %= mod ; 
+	    }
+	    return dp[n] ; 
+	}
+}
