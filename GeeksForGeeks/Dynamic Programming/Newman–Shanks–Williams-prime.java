2bdecc00d4b8b5047a7f846550d0c76a0e132eb7
Pradhuman Shankar <42096528+pradhuman2022@users.noreply.github.com>
2020-01-28 23:50:50 +0530

    Update Newman–Shanks–Williams-prime.java
    In mathematics, a Newman–Shanks–Williams prime (NSW prime) is a prime number p which can be written in the form:
    { \Huge S_{2m+1} = \frac{ {(1+\sqrt{2})^{2m+1} + (1-\sqrt{2})^{2m+1} } }{2} }
    
    Recurrence relation for Newman–Shanks–Williams prime is:
    
    S_0 = 1
    S_1 = 1
    S_n = 2*S_{n-1} + S{n-2}
    The first few terms of the sequence are 1, 1, 3, 7, 17, 41, 99, ….

--- GeeksForGeeks/Dynamic Programming/Newman–Shanks–Williams-prime.java
+++ GeeksForGeeks/Dynamic Programming/Newman–Shanks–Williams-prime.java
@@ -16,3 +16,14 @@ class GFG {
 	   System.out.println(dp[n]) ;   
 	}
 }
+/*
+In mathematics, a Newman–Shanks–Williams prime (NSW prime) is a prime number p which can be written in the form:
+{ \Huge S_{2m+1} = \frac{ {(1+\sqrt{2})^{2m+1} + (1-\sqrt{2})^{2m+1} } }{2} }
+
+Recurrence relation for Newman–Shanks–Williams prime is:
+
+S_0 = 1
+S_1 = 1
+S_n = 2*S_{n-1} + S{n-2}
+The first few terms of the sequence are 1, 1, 3, 7, 17, 41, 99, ….
+*/
