e762eb38a6f564e814cc994e4c3e6efa2d5d35d2
Pradhuman Shankar <42096528+pradhuman2022@users.noreply.github.com>
2020-01-30 23:55:17 +0530

    Create longestCommonSubsequence.java
    https://www.geeksforgeeks.org/longest-common-subsequence-dp-4/

---
+++ GeeksForGeeks/Dynamic Programming/longestCommonSubsequence.java
@@ -0,0 +1,39 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+		Scanner sc = new Scanner(System.in) ; 
+		int T = sc.nextInt() ; 
+		
+		while(T-- >0){
+		    int n = sc.nextInt();
+		    int m = sc.nextInt() ; 
+		    sc.nextLine(); 
+		    String s1 = sc.nextLine() ; 
+		    String s2 = sc.nextLine() ; 
+		    System.out.println(longestSubsequence(s1, s2, n, m)) ; 
+		}
+	}
+  //iterative way
+     static int longestSubsequence(String s1, String s2, int n , int m){
+         int dp[][] = new int[n + 1][m + 1] ; 
+         
+         for(int i = 0 ; i <= n ; i++){
+             for(int j = 0; j <= m ; j++){
+                 if(i == 0 || j == 0)//if we have nothing in any one of string then ans should be 0.
+                   dp[i][j] = 0 ; 
+                 else if(s1.charAt(i - 1) == s2.charAt(j - 1))// if both char is same then we need to add a char thats why +1 
+                  dp[i][j] = dp[i - 1][j - 1] + 1 ;  //and get max longest subsequence till [i - 1, j - 1] and add it.
+                 else
+                  dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) ; //if both char is diff then we need to check if any of [i - 1, j] 
+                                                                   //and [i, j - 1] having longest subsequence.
+             }
+         }
+         return dp[n][m] ;
+     }
+}
