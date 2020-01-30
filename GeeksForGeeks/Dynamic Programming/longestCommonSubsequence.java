4ca42b332d0e2e2bdbfae9a0925e3f160e2f373e
Pradhuman Shankar <42096528+pradhuman2022@users.noreply.github.com>
2020-01-31 00:30:46 +0530

    Update longestCommonSubsequence.java

--- GeeksForGeeks/Dynamic Programming/longestCommonSubsequence.java
+++ GeeksForGeeks/Dynamic Programming/longestCommonSubsequence.java
@@ -36,4 +36,26 @@ class GFG {
          }
          return dp[n][m] ;
      }
+	//space optimized
+	static int longestSubsequence(String s1, String s2, int n , int m){
+         int dp[][] = new int[2][m + 1] ; 
+         int bi = 0 ; 
+         
+         for(int i = 0 ; i <= n ; i++){
+             
+             //just a way to toggle array.
+             bi = i & 1 ; 
+             
+             
+             for(int j = 0; j <= m ; j++){
+                 if(i == 0 || j == 0)
+                   dp[bi][j] = 0 ; 
+                 else if(s1.charAt(i - 1) == s2.charAt(j - 1)) 
+                   dp[bi][j] = dp[1 - bi][j - 1] + 1 ;
+                 else
+                  dp[bi][j] = Math.max(dp[1 - bi][j], dp[bi][j - 1]) ; 
+             }
+         }
+         return dp[bi][m] ;
+     }
 }
