1efa96353b78cbf169ac2d5fbe695eafe074246e
Pradhuman Shankar <42096528+pradhuman2022@users.noreply.github.com>
2020-02-07 14:09:51 +0530

    Update maxPathSumInMat.java

--- GeeksForGeeks/Dynamic Programming/maxPathSumInMat.java
+++ GeeksForGeeks/Dynamic Programming/maxPathSumInMat.java
@@ -24,9 +24,9 @@ class GFG {
 	     dp[0][i + 1] = mat[0][1] ; 
 	     
 	    for(int i = 1 ; i < n; i++){
-	        for(int j = 1 ; j <= m ; i++)
+	        for(int j = 1 ; j <= m ; j++)
 	          {
-	              dp[i][j] = Math.max(dp[i - 1][j], Math.max(dp[i- 1][j + 1], dp[i - 1]][j - 1]) ;
+	              dp[i][j] = Math.max(dp[i - 1][j], Math.max(dp[i- 1][j + 1], dp[i - 1]][j - 1]) + mat[i - 1][j - 1];
 	          }
 	    } 
 	    
