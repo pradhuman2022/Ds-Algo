225d27e00ea86b28c3877ee82a6eea1b8dbab61b
Pradhuman Shankar <42096528+pradhuman2022@users.noreply.github.com>
2020-02-15 16:14:30 +0530

    Update countWaysTobuildStreet.java

--- GeeksForGeeks/Dynamic Programming/countWaysTobuildStreet.java
+++ GeeksForGeeks/Dynamic Programming/countWaysTobuildStreet.java
@@ -18,7 +18,7 @@ class GFG {
         for (int i = 2; i <= n; i++){
             
             //if atleast one house and one office 
-            dp[1][i] = dp[0][i - 1] * 2 + dp[i - 1][1] ; 
+            dp[1][i] = dp[0][i - 1] * 2 + dp[1][i - 1] ; 
             dp[0][i] = dp[0][i - 1] + dp[1][i - 1];
         }
         return dp[1][n] + dp[0][n] ;
