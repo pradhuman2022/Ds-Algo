32915c5f4a8a7fe2cedae35d63d00ef420ef3640
Pradhuman Shankar <42096528+pradhuman2022@users.noreply.github.com>
2020-02-22 00:44:16 +0530

    BFS of graph
    https://practice.geeksforgeeks.org/problems/bfs-traversal-of-graph/1

---
+++ GeeksForGeeks/Graph/BFS.java
@@ -0,0 +1,21 @@
+class Traversal
+{
+    static void bfs(int s, ArrayList<ArrayList<Integer>> list, boolean visited[], int nov)
+    {
+       // add your code here
+       Queue<Integer> q = new ArrayDeque<Integer>();
+       q.offer(s) ; 
+       while(!q.isEmpty()){
+            int vertex = q.poll();
+            if(!visited[vertex])
+                System.out.print(vertex+" ");
+            visited[vertex] = true ;
+            for(Integer adj : list.get(vertex)){
+                if(!visited[adj]){
+                    q.offer(adj); 
+                }
+            }
+       }
+    }
+}
+
