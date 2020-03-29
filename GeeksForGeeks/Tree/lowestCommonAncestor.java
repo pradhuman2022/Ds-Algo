67defd3e7be3af66568de57f1ab3680470ddc050
Pradhuman Shankar <42096528+pradhuman2022@users.noreply.github.com>
2020-03-29 16:50:31 +0530

    Lowest Common Ancestor in a Binary Tree.
    https://practice.geeksforgeeks.org/problems/lowest-common-ancestor-in-a-binary-tree/1

---
+++ GeeksForGeeks/Tree/lowestCommonAncestor.java
@@ -0,0 +1,26 @@
+class Tree
+{
+
+/* If n1 and n2 are present, return reference
+   to LCA. If both are not present, return 
+   null,. Else if left subtree contains any 
+   of them return pointer to left.*/
+	Node lca(Node root, int n1,int n2)
+	{
+		// Your code here
+		if(root == null)
+		    return null ; 
+		    
+	    if(root.data == n1 || root.data == n2)
+	        return root;
+	    
+	    Node left = lca(root.left, n1, n2);
+	    Node right = lca(root.right, n1, n2);
+	    
+	    if(left != null && right != null) 
+	        return root;
+	    
+	    return left != null ? left : right;  
+	}
+}
+
