0b692aaa6529865de8a56ab79386bb474d35ed1e
Pradhuman Shankar <42096528+pradhuman2022@users.noreply.github.com>
2020-04-12 23:33:30 +0530

    Middle Class.
    https://codeforces.com/contest/1334/problem/B

--- Codeforces/Div2/B/R85.java
+++ Codeforces/Div2/B/R85.java
@@ -1,89 +1,170 @@
     import java.util.*;
-    import java.io.BufferedReader; 
-    import java.io.IOException; 
-    import java.io.InputStreamReader; 
-    import java.util.Scanner; 
-    import java.util.StringTokenizer; 
-    public class MyCode {
-       
-       static class FastReader 
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+import java.util.Scanner; 
+import java.util.StringTokenizer; 
+public class MyCode {
+   
+   static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
         { 
-            BufferedReader br; 
-            StringTokenizer st; 
-      
-            public FastReader() 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
             { 
-                br = new BufferedReader(new
-                         InputStreamReader(System.in)); 
-            } 
-      
-            String next() 
-            { 
-                while (st == null || !st.hasMoreElements()) 
-                { 
-                    try
-                    { 
-                        st = new StringTokenizer(br.readLine()); 
-                    } 
-                    catch (IOException  e) 
-                    { 
-                        e.printStackTrace(); 
-                    } 
-                } 
-                return st.nextToken(); 
-            } 
-      
-            int nextInt() 
-            { 
-                return Integer.parseInt(next()); 
-            } 
-      
-            long nextLong() 
-            { 
-                return Long.parseLong(next()); 
-            } 
-      
-            double nextDouble() 
-            { 
-                return Double.parseDouble(next()); 
-            } 
-      
-            String nextLine() 
-            { 
-                String str = ""; 
                 try
                 { 
-                    str = br.readLine(); 
+                    st = new StringTokenizer(br.readLine()); 
                 } 
-                catch (IOException e) 
+                catch (IOException  e) 
                 { 
                     e.printStackTrace(); 
                 } 
-                return str; 
             } 
+            return st.nextToken(); 
         } 
-       public static void main(String ar[]) {
-        
-         FastReader sc = new FastReader();
-         int testCases = sc.nextInt();
-         while(testCases-->0) {
-            int n = sc.nextInt();
-            int x = sc.nextInt();
-            int wealthOfPerson[] = new int[n];
-            for(int i = 0; i < n; i++) {
-               wealthOfPerson[i] = sc.nextInt();
-            }
-            System.out.println(getNumberOfWealthyPersons(wealthOfPerson, n, x));
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    } 
+   public static void main(String ar[]) {
+    
+     FastReader sc = new FastReader();
+     int testCases = sc.nextInt();
+     while(testCases-->0) {
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int sum = 0 ; 
+        int wealthOfPerson[] = new int[n];
+        for(int i = 0; i < n; i++) {
+           wealthOfPerson[i] = sc.nextInt();
+           sum += wealthOfPerson[i];
+        }
+        if(sum / n >= x) 
+            System.out.println(n);
+        else
+        System.out.println(getNumberOfWealthyPersons(wealthOfPerson, n, x));
+     }
+   }
+     static long getNumberOfWealthyPersons(int wealthOfPerson[], int n, int x){
+         long countWealthyPersons = 0;
+         sort(wealthOfPerson, 0, n - 1);
+         long sumOfWealth = 0; 
+         int wealth = n - 1;
+         while(wealth >= 0) {
+            if(sumOfWealth + wealthOfPerson[wealth] >= x * (countWealthyPersons + 1)) {
+             sumOfWealth += wealthOfPerson[wealth--] ; 
+             countWealthyPersons++;
+            }else break ; 
          }
-       }
-         static long getNumberOfWealthyPersons(int wealthOfPerson[], int n, int x){
-             long countWealthyPersons = 0;
-             Arrays.sort(wealthOfPerson);
-             long sumOfWealth = 0; 
-             for(int wealth = n - 1; wealth >= 0 && sumOfWealth + wealthOfPerson[wealth] >= x * (countWealthyPersons + 1); wealth--) {
-                 sumOfWealth += wealthOfPerson[wealth] ; 
-                 countWealthyPersons++;
-             }
-             return countWealthyPersons;
-         }
-    }
+         return countWealthyPersons;
+     }
+     static  void merge(int arr[], int l, int m, int r) 
+    { 
+        // Find sizes of two subarrays to be merged 
+        int n1 = m - l + 1; 
+        int n2 = r - m; 
+  
+        /* Create temp arrays */
+        int L[] = new int [n1]; 
+        int R[] = new int [n2]; 
+  
+        /*Copy data to temp arrays*/
+        for (int i=0; i<n1; ++i) 
+            L[i] = arr[l + i]; 
+        for (int j=0; j<n2; ++j) 
+            R[j] = arr[m + 1+ j]; 
+  
+  
+        /* Merge the temp arrays */
+  
+        // Initial indexes of first and second subarrays 
+        int i = 0, j = 0; 
+  
+        // Initial index of merged subarry array 
+        int k = l; 
+        while (i < n1 && j < n2) 
+        { 
+            if (L[i] <= R[j]) 
+            { 
+                arr[k] = L[i]; 
+                i++; 
+            } 
+            else
+            { 
+                arr[k] = R[j]; 
+                j++; 
+            } 
+            k++; 
+        } 
+  
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+            k++; 
+        } 
+  
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) 
+        { 
+            arr[k] = R[j]; 
+            j++; 
+            k++; 
+        } 
+    } 
+  
+    // Main function that sorts arr[l..r] using 
+    // merge() 
+    static void sort(int arr[], int l, int r) 
+    { 
+        if (l < r) 
+        { 
+            // Find the middle point 
+            int m = (l+r)/2; 
+  
+            // Sort first and second halves 
+            sort(arr, l, m); 
+            sort(arr , m+1, r); 
+  
+            // Merge the sorted halves 
+            merge(arr, l, m, r); 
+        } 
+    } 
+}
