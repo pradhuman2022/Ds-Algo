5fb45f13b1b07287608d4039cf64fad0f4da77ae
Pradhuman Shankar <42096528+pradhuman2022@users.noreply.github.com>
2020-05-03 23:24:03 +0530

    Same Tree.
    https://leetcode.com/problems/same-tree/

---
+++ LeetCode/Tree/isSameTree.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+            if(p == null && q != null || p != null && q == null)
+                return false; 
+            if(p == null && q == null)
+                return true;
+            return isSameTree(p.left, q.left) && p.val == q.val && isSameTree(p.right, q.right); 
+    }
+    
+}
