7f78dd9546031799c570a66451829d18b7150571
Pradhuman Shankar <42096528+pradhuman2022@users.noreply.github.com>
2021-02-03 03:26:03 +0530

    1368. Minimum Cost to Make at Least One Valid Path in a Grid
    https://leetcode.com/problems/minimum-cost-to-make-at-least-one-valid-path-in-a-grid/
    It uses 0-1 BFS by assuming if we modify any cell then we will have an extra edge with 1 weight.

---
+++ Graph Concepts/minimumCostToMakeValidPath.java
@@ -0,0 +1,60 @@
+class Solution {
+    
+    static int [][] grids;
+    public int minCost(int[][] grid) {
+        int steps[][] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        ArrayDeque<Pair> q = new ArrayDeque<>();
+        q.add(new Pair(0, 0));
+        int m = grid.length;
+        int n = grid[0].length;
+        int dist[][] = new int[m][n];
+        
+        grids = grid;
+        for (int mi[] : dist)
+            Arrays.fill(mi, Integer.MAX_VALUE);
+        
+        dist[0][0] = 0;
+        
+        while(!q.isEmpty()) {
+            Pair p = q.poll();
+            label: for (int i = 1; i <= 4; i++) {
+                int newX = p.x + steps[i - 1][0];
+                int newY = p.y + steps[i - 1][1];
+                if(!isValid(newX, newY)) {
+                    continue label;
+                }
+                
+                if(i != grid[p.x][p.y]) {
+                    if (dist[newX][newY] > 1 + dist[p.x][p.y]) {
+                        dist[newX][newY] = 1 + dist[p.x][p.y];
+                         System.out.println(grid[newX][newY]+"not mactech"+i);
+                        q.addLast(new Pair(newX, newY));
+                    }
+                } else {
+                    if (dist[newX][newY] > dist[p.x][p.y]) {
+                        dist[newX][newY] = dist[p.x][p.y];
+                        System.out.println(grid[newX][newY]+"mactech"+i);
+                        q.addFirst(new Pair(newX, newY));
+                    }
+                }
+            }
+        }
+       
+        return dist[m - 1][n - 1] ;
+    }
+    
+    public static boolean isValid(int x , int y) {
+        if(x < 0 || y < 0 || grids.length <= x || grids[0].length <= y)
+            return false;
+        return true;
+    }
+    
+    static class Pair {
+        int x;
+        int y;
+        Pair(int x, int y) {
+            this.x = x; 
+            this.y = y;
+        }
+    } 
+}
