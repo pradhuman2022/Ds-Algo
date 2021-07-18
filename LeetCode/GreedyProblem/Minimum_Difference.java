42575b910ba05089294293ebff6a732b6896e649
Pradhuman Shankar <42096528+pradhuman2022@users.noreply.github.com>
2021-07-18 20:43:19 +0530

    1509. Minimum Difference Between Largest and Smallest Value in Three Moves
    https://leetcode.com/problems/minimum-difference-between-largest-and-smallest-value-in-three-moves/

--- LeetCode/GreedyProblem/Minimum_Difference.java
+++ LeetCode/GreedyProblem/Minimum_Difference.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int minDifference(int[] nums) {
+   
+        int n = nums.length - 1;
+        if(nums.length < 5) 
+            return 0;
+        Arrays.sort(nums);
+        
+        int ans = Math.abs(nums[3] - nums[n]);
+        ans = Math.min(ans, Math.abs(nums[n - 3] - nums[0]));
+        ans = Math.min(ans, Math.abs(nums[n - 1] - nums[2]));
+        ans = Math.min(ans, Math.abs(nums[1] - nums[n - 2]));
+        
+        return ans;
+    }
+}
