a49134b60fa8362102c7b2a188a36c2a9880ad81
Pradhuman Shankar <42096528+pradhuman2022@users.noreply.github.com>
2021-07-23 00:17:45 +0530

    1526. Minimum Number of Increments on Subarrays to Form a Target Array
    https://leetcode.com/problems/minimum-number-of-increments-on-subarrays-to-form-a-target-array/

--- LeetCode/GreedyProblem/minimumIncrement.java
+++ LeetCode/GreedyProblem/minimumIncrement.java
@@ -1,1 +1,11 @@
-
+class Solution {
+    public int minNumberOperations(int[] target) {
+        Stack<Integer> stack = new Stack();
+        int count = target[0];
+        for(int i = 1; i < target.length; i++) { 
+            if(target[i] > target[i - 1]) 
+                count += target[i] - target[i - 1];
+        }  
+        return count;
+    }
+}
