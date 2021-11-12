4581a35aeb02af52bbf575ecf5cf54c6105bc167
Pradhuman Shankar <42096528+pradhuman2022@users.noreply.github.com>
2021-11-12 19:59:07 +0530

    Refractoring

--- Graph Concepts/alienDictionary.java
+++ Graph Concepts/alienDictionary.java
@@ -1,72 +1,79 @@
 class Solution {
-     Map<Character, List<Character>> adjList;
+    
+    int visited[];
+    HashMap<Character, Set<Character>> graph;
+    HashSet<Character> set ;
+    Stack<Character> st;
     public String alienOrder(String[] words) {
-       
-        Map<Character, List<Character>> adjList = new HashMap<>();
-        Map<Character, Integer> charSet = new HashMap<>();
-        Set<Character> visited = new HashSet<>();
         
-        for(int i = 0; i < words.length; i++)  {
-            for(int j = 0 ; j < words[i].length(); j++) 
-                charSet.put(words[i].charAt(j), 0);
-        }
+        graph = new HashMap<>();
+        visited = new int[26];
+        set = new HashSet<>();
+        if (!buildGraph(words, graph, set))
+            return "";
         
-        for (int i = 0; i < words.length - 1; i++) {
-            String word1 = words[i];
-            String word2 = words[i + 1];
-            
-            if(word1.length() > word2.length() && word1.startsWith(word2))
-                return "";
-            
-            for (int j = 0; j < Math.min(word1.length(), word2.length()); j++) {
-                if(word1.charAt(j) != word2.charAt(j)) {
-                    List<Character> list = adjList.getOrDefault(word1.charAt(j), new ArrayList<>());
-                    list.add(word2.charAt(j));
-                    adjList.put(word1.charAt(j), list);
-                    break;
-                }
+        st = new Stack<>();
+
+        for (char c: set) {
+            if (visited[c - 'a'] == 0) {
+                if (dfs(c))
+                    return "";
             }
         }
         
-        this.adjList = adjList;
         StringBuilder ans = new StringBuilder();
-        Stack<Character> st = new Stack<>();
-        for (int i = 0 ; i < 26; i++) {
-            int onStack[] = new int[26];
-            if(!visited.contains((char)(i + 97)) && charSet.containsKey((char)(i + 97))) {
-               if(topoSort(st, (char) (i + 97), visited, onStack))
-                   return "";
-            }
-        }
-        visited = null;
-        adjList = null;
-        charSet = null;
-        System.gc();
-        while(!st.isEmpty()) {
+        
+        while(!st.isEmpty())
             ans.append(st.pop());
-        }
         
         return ans.toString();
     }
     
-    public boolean topoSort(Stack<Character> st, Character source, Set<Character> visited, int onStack[]) {
+    public boolean dfs(char source) {
         
-        if(onStack[source - 97] == 1)
-            return true;
+        visited[source - 'a'] = 1;
+        System.out.println(source+ " " + graph);
         
-        onStack[source - 97] = 1;
-        for (Character nextChar: adjList.getOrDefault(source, new ArrayList<>())) {
-            if(!visited.contains(nextChar)){
-                if(topoSort(st, nextChar, visited, onStack))
-                    return true;
-                System.out.println(nextChar);
-            }
+        for (char child: graph.getOrDefault(source, new HashSet<>())) {
+            if (visited[child - 'a'] == 1 || (visited[child - 'a'] == 0 && dfs(child)))
+                return true;   
         }
         
-        onStack[source - 97] = 0;
-        if(!visited.contains(source))
-            st.push(source); 
-        visited.add(source);
+        visited[source - 'a'] = 2;
+        st.push(source);
+        
         return false;
+    }
+    
+    
+    public boolean buildGraph(String [] words, HashMap<Character, Set<Character>> graph, HashSet<Character> set) {
+        
+        
+        for (int i = 0; i < words.length; i++) {
+             
+            String word1 = words[i];
+     
+            for (char c: words[i].toCharArray())
+                set.add(c);
+            
+            if (i == words.length - 1) {
+                break;
+            }
+            
+            String word2 = words[i + 1];
+            
+            if(word1.length() > word2.length() && word1.startsWith(word2))
+                return false;
+        
+            for (int j = 0; j < Math.min(word1.length(), word2.length()); j++) {
+                if (word1.charAt(j) != word2.charAt(j)) {
+                    Set<Character> adjList = graph.getOrDefault(word1.charAt(j), new HashSet<>());
+                    adjList.add(word2.charAt(j));
+                    graph.put(word1.charAt(j), adjList);
+                    break;
+                }  
+            }
+        }
+        return true;
     }
 }
